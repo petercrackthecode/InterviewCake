e90b3bf79c705f8c91cb816d95eaaa0d1a3c3a69
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-05-08 18:20:38 -0700

    Started working on getMaxProfit.cpp

---
+++ getMaxProfit.cpp
@@ -0,0 +1,8 @@
+// link: https://www.interviewcake.com/question/cpp/stock-price?course=fc1&section=greedy
+int getMaxProfit(const std::vector<int>& stockPrices)
+{
+    // calculate the max profit
+    
+
+    return 0;
+}
