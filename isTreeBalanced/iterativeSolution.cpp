9020a2e0abbc271d2778c4205a407a0f59321769
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-09-24 12:52:18 -0700

    Finished the iterative solution __ Finally

--- isTreeBalanced/iterativeSolution.cpp
+++ isTreeBalanced/iterativeSolution.cpp
@@ -29,15 +29,43 @@ public:
 bool isBalanced(const BinaryTreeNode *treeRoot) {
     // a tree with no nodes is superbalanced, since there are no leaves!
     std::stack<const BinaryTreeNode*> nodesStack;
-    std::map<const BinaryTreeNode *, bool> visited;
-    const BinaryTreeNode * node = treeRoot;
+    std::map<BinaryTreeNode *, bool> visited;
+    BinaryTreeNode * node = new BinaryTreeNode(treeRoot->value_);
+    node->left_  = treeRoot->left_;
+    node->right_ = treeRoot->right_;
 
     int minHeight = 1, maxHeight = 1;
 
     bool balanced = true;
 
-    while (!node || !nodesStack.empty()) {
-    	
+    while (node || !nodesStack.empty()) {
+        // take all the nodes on the left
+        while (node) {
+            nodesStack.push(node);
+            visited[node] = true;
+            node = node->left_;
+        }
+        
+        if (!nodesStack.top()->left_ && !nodesStack.top()->right_) {
+            int currentHeight = nodesStack.size();
+            
+            if (minHeight == 1) 
+                minHeight = currentHeight;
+            else minHeight = std::min(minHeight, currentHeight);
+            maxHeight = std::max(maxHeight, currentHeight);
+
+            if (maxHeight - minHeight > 1) {
+                balanced = false;
+                break;
+            }
+        }
+        
+        if (!visited[nodesStack.top()->right_] && nodesStack.top()->right_)  {
+            node = nodesStack.top()->right_;
+        }
+        else {
+            nodesStack.pop();
+        }
     }
 
     return balanced;
