36a255ade0cc3604dda5d8d1b493a461b6e32835
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-10-29 14:14:40 -0700

    better version of findDuplicate.cpp

--- findDuplicate.cpp
+++ findDuplicate.cpp
@@ -6,38 +6,49 @@
 
 using namespace std;
 
-unsigned int findDuplicate(const vector<unsigned int>& intVector)
-{
-    // find a number that appears more than once ... in O(n) time
-    int length = intVector.size(),
-        head = intVector[length - 1],
-        headPos = length;
-        
-    int tempPointer = head;
-    
-    for (int i = 0; i < length; ++i) {
-        tempPointer = intVector[tempPointer - 1];
+unsigned int findDuplicate(const std::vector<unsigned int> &intVector) {
+    const unsigned int n = static_cast<unsigned int>(intVector.size() - 1);
+
+    // STEP 1: GET INSIDE A CYCLE
+    // start at position n + 1 and walk n steps to find a position guaranteed to be in a cycle
+    unsigned int positionInCycle = n + 1;
+    for (unsigned int i = 0; i < n; ++i) {
+        // we substract 1 from the current position to step ahead:
+        // the 2nd *position* in an array is *index* 1
+        positionInCycle = intVector[positionInCycle - 1];
     }
-    // now we inside the cycle
-    int start = tempPointer,
-        cycleLength = 1;
-    
-    tempPointer = intVector[start - 1];
-    while (tempPointer != start) {
-        tempPointer = intVector[tempPointer - 1];
-        ++cycleLength;
+
+    // STEP 2: FIND THE LENGTH OF THE CYCLE
+    // find the length of the cycle by remembering a position in the cycle
+    // and counting the steps it takes to get back to that position
+    unsigned int rememberedPositionInCycle = positionInCycle,
+                 currentPositionInCycle = intVector[positionInCycle - 1],
+                 cycleStepCount = 1;
+
+    while (currentPositionInCycle != rememberedPositionInCycle) {
+        currentPositionInCycle = intVector[currentPositionInCycle - 1];
+        ++cycleStepCount;
     }
-    
-    int trackerPointer = head;
-    
-    for (int i = 0; i < cycleLength; ++i)   {
-        trackerPointer = intVector[trackerPointer - 1];
+
+    // STEP 3: FIND THE FIRST NODE OF THE CYCLE
+    // start two pointers
+    // (1) at position n + 1
+    // (2) ahead of position n + 1 as many steps as the cycle's length
+    unsigned int pointerStart = n + 1,
+                 pointerAhead = n + 1;
+    // move pointerAhead cycleStepCount steps 
+    for (std::size_t i = 0; i < cycleStepCount; ++i)    {
+        pointerAhead = intVector[pointerAhead - 1];
     }
-    
-    while (head != trackerPointer)   {
-        head = intVector[head - 1];
-        trackerPointer = intVector[trackerPointer - 1];
+
+    // advance until the pointers are in the same position
+    // which is the first node in the cycle.
+    while (pointerStart != pointerAhead)    {
+        pointerStart = intVector[pointerStart - 1];
+        pointerAhead = intVector[pointerAhead - 1];
     }
-    
-    return head;
+
+    // since there are multiple values pointing to the first node in the cycle,
+    // its position is a duplicate in our array
+    return pointerStart;
 }
