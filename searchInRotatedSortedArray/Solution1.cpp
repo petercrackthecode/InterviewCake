42f3a452231d4a2addc0a17c0101edf4522b3d55
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-11-20 11:51:04 -0800

    Quirky problem

---
+++ searchInRotatedSortedArray/Solution1.cpp
@@ -0,0 +1,12 @@
+// link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        for (auto num : nums) {
+            if (num == target)
+                return true;
+        }
+        
+        return false;
+    }
+};
---
+++ searchInRotatedSortedArray/Solution2.cpp
@@ -0,0 +1,2 @@
+// link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+// To be solved, faster than the solution 1.
