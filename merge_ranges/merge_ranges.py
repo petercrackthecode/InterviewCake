396631cc578ff85de0ea0a94f00029a5969d0c3a
Peter Nguyen <peternguyenforwork@gmail.com>
2022-06-25 15:58:34 -0700

    remove redundant try-except and print statements

--- merge_ranges/merge_ranges.py
+++ merge_ranges/merge_ranges.py
@@ -1,6 +1,6 @@
 import functools
 
-meeting_blocks = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+meeting_blocks = [(1, 10), (2, 6), (3, 5), (7, 9)]
 
 
 def comparator(m1, m2):
@@ -13,7 +13,6 @@ def comparator(m1, m2):
 
 
 def is_intercepted(m1, m2) -> bool:
-    print("m1 = ", m1, ", m2 = ", m2)
     return (m1[0] <= m2[0] and m2[0] <= m1[1]) or (m2[0] <= m1[0] and m1[0] <= m2[1])
 
 
@@ -23,7 +22,6 @@ def merge_ranges(meetings):
 
     # Merge meeting ranges
     sorted_meetings = sorted(meetings, key=functools.cmp_to_key(comparator))
-    print("sorted_meetings = ", sorted_meetings)
 
     ans = [sorted_meetings[0]]
 
@@ -31,15 +29,12 @@ def merge_ranges(meetings):
         ans_length = len(ans)
         # two meeting blocks ain't intercepted. We're not merging.
         # Just add a new block to the end of the answer array
-        try:
-            if not is_intercepted(ans[ans_length - 1], sorted_meetings[i]):
-                ans.append(sorted_meetings[i])
-                continue
-            new_start_time = min(ans[ans_length - 1][0], sorted_meetings[i][0])
-            new_end_time = max(ans[ans_length - 1][1], sorted_meetings[i][1])
-            ans[ans_length - 1] = (new_start_time, new_end_time)
-        except:
-            print(f"Failed at index {i}, ans = ", ans)
+        if not is_intercepted(ans[ans_length - 1], sorted_meetings[i]):
+            ans.append(sorted_meetings[i])
+            continue
+        new_start_time = min(ans[ans_length - 1][0], sorted_meetings[i][0])
+        new_end_time = max(ans[ans_length - 1][1], sorted_meetings[i][1])
+        ans[ans_length - 1] = (new_start_time, new_end_time)
 
     return ans
 
