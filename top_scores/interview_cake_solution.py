8a2740ae27a2f005b3a78a5ce11322da6e17bcad
Peter Nguyen <peternguyenforwork@gmail.com>
2022-07-14 18:48:29 -0700

    add interview cake's solution, pretty similar to mine.

---
+++ top_scores/interview_cake_solution.py
@@ -0,0 +1,23 @@
+# https://www.interviewcake.com/question/python3/top-scores?course=fc1&section=sorting-searching-logarithms
+
+def sort_scores(unsorted_scores, highest_possible_score):
+    # List of 0s at indices 0..highest_possible_score
+    score_counts = [0] * (highest_possible_score+1)
+
+    # Populate score_counts
+    for score in unsorted_scores:
+        score_counts[score] += 1
+
+    # Populate the final sorted list
+    sorted_scores = []
+
+    # For each item in score_counts
+    for score in range(len(score_counts) - 1, -1, -1):
+        count = score_counts[score]
+
+        # For the number of times the item occurs
+        for time in range(count):
+            # Add it to the sorted list
+            sorted_scores.append(score)
+
+    return sorted_scores
